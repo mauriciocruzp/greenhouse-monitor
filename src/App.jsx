169f928292c0cf6ccff5a7bf6f6523d02894bccb
Mauricio Cruz <mawcp7@gmail.com>
2023-03-26 14:24:24 -0600

    add grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,19 @@ function App() {
   }, []);
   console.log(data);
   return (
-    <div className="w-96">
-      <div className="h-full w-full">
-        <div className="mx-8 h-full flex items-center justify-center flex-col">
-          <TemperatureScreen temperature={data.temperature} />
-          <div className="mt-4 w-full flex flex-row space-x-2">
-            <HumidityScreen humidity={data.humidity} />
-            <PressureScreen pressure={data.pressure} />
+    <div className="container mx-auto">
+      <div className="w-auto grid lg:grid-cols-12 sm:grid-cols-4 gap-x-4">
+        <div className="w-auto h-full lg:col-span-4 lg:col-start-5 sm:col-span-4 ">
+          <div className="w-auto h-full grid grid-cols-4 gap-4">
+            <div className="col-span-4">
+              <TemperatureScreen temperature={data.temperature} />
+            </div>
+            <div className="col-span-2">
+              <HumidityScreen humidity={data.humidity} />
+            </div>
+            <div className="col-span-2">
+              <PressureScreen pressure={data.pressure} />
+            </div>
           </div>
         </div>
       </div>
--- src/containers/PressureScreen/PressureScreen.jsx
+++ src/containers/PressureScreen/PressureScreen.jsx
@@ -1,7 +1,9 @@
 function PressureScreen({pressure}) {
-    const mmghPressure = pressure * 760;
+    let mmghPressure = pressure * 760;
+    mmghPressure = Math.round((mmghPressure + Number.EPSILON) * 100) / 100;
+    pressure = Math.round((pressure + Number.EPSILON) * 100) / 100;
     return (
-        <div className="bg-primary-dark w-1/2 h-40 flex items-center rounded-3xl justify-center">
+        <div className="bg-primary-dark w-full h-40 flex items-center rounded-3xl justify-center">
             <div className="flex flex-col">
                 <div className="text-primary-purple w-40 h-6 text-center text-xl">Presión</div>
                 <div className="text-primary-purple w-40 h-6 text-center text-xl jus">{pressure}atm/</div>
