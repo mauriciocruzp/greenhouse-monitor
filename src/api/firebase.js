147e21fa6699d0f0fa7cd9ea87e5e29a3cd5f268
Mauricio Cruz <92227312+mauriciocruzp@users.noreply.github.com>
2023-03-26 21:27:02 -0600

    delete firebase creadentials

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRWLGLgA6l13Dosk01BNXlpyCRCUJi_OE",
-    authDomain: "sistemas-d4fb0.firebaseapp.com",
-    databaseURL: "https://sistemas-d4fb0-default-rtdb.firebaseio.com",
-    projectId: "sistemas-d4fb0",
-    storageBucket: "sistemas-d4fb0.appspot.com",
-    messagingSenderId: "844948877282",
-    appId: "1:844948877282:web:bc9567a7723ea4fbca53bb",
-    measurementId: "G-RJ2JK9D1NH"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
